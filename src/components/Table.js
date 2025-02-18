3f49d361e620658768419d4030643fe5719edec2
Someswarklizos2001 <someswar@klizos.com>
2025-02-18 22:45:09 +0530

    updated

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,11 +2,14 @@ import Table from "react-bootstrap/Table";
 import styles from "../styles/table.module.css";
 import { CircularProgress } from "./CircularProgress";
 import { useState } from "react";
+import { FaArrowRight } from "react-icons/fa";
 
 export const TableLayout = ({ brokers, load }) => {
   const [currentPage, setCurrentPage] = useState(0);
   let total = brokers.length;
-  let totalPage = Math.ceil(total / 10);
+  let totalPage = Math.ceil(total / 8);
+  let maxSlide=Math.ceil(totalPage/3);
+  const [slide,setSlide]=useState(0);
 
   return (
     <div className={styles.tableContainer}>
@@ -16,7 +19,7 @@ export const TableLayout = ({ brokers, load }) => {
         </div>
       ) : (
         <Table striped bordered hover className={styles.table}>
-          <thead >
+          <thead>
             <tr>
               <th>BROKER NAME</th>
               <th>EMAIL</th>
@@ -28,7 +31,7 @@ export const TableLayout = ({ brokers, load }) => {
           <tbody>
             {brokers.length > 0 ? (
               brokers
-                ?.slice(currentPage * 10, currentPage * 10 + 10)
+                ?.slice(currentPage * 8, currentPage * 8 + 8)
                 .map((data, index) => (
                   <tr key={index}>
                     <td>
@@ -48,16 +51,21 @@ export const TableLayout = ({ brokers, load }) => {
 
       <div className={styles.pagination}>
         {Array(totalPage)
+          .slice(0,3)
           .fill()
           .map((data, index) => (
             <button
-             
-              onClick={(e) => setCurrentPage(index)}
-              className={index===currentPage? styles.clickedPaginationButton:styles.paginationButton}
+              onClick={(e) => setCurrentPage(index+slide)}
+              className={
+                index+slide === currentPage
+                  ? styles.clickedPaginationButton
+                  : styles.paginationButton
+              }
             >
-              {index + 1}
+              {index+1+slide}
             </button>
           ))}
+          <FaArrowRight onClick={(e)=>setSlide(slide+3)}/>
       </div>
     </div>
   );
