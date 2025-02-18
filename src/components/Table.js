528b4a92d00d582fc2e60f37650c2a4ce47983c7
Someswarklizos2001 <someswar@klizos.com>
2025-02-18 22:52:15 +0530

    updated

--- src/components/Table.js
+++ src/components/Table.js
@@ -62,7 +62,7 @@ export const TableLayout = ({ brokers, load }) => {
                   : styles.paginationButton
               }
             >
-              {index+1+slide} "segseg"
+              {index+1+slide} "segseg "
             </button>
           ))}
           <FaArrowRight onClick={(e)=>setSlide(slide+3)}/>
