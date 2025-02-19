22f93b935c40aee50cb5e840ea52bad4bc3b6077
Someswarklizos2001 <someswar@klizos.com>
2025-02-19 09:13:58 +0530

    updated

--- src/components/Table.js
+++ src/components/Table.js
@@ -15,7 +15,7 @@ export const TableLayout = ({ brokers, load }) => {
     <div className={styles.tableContainer}>
       {load ? (
         <div className={styles.center}>
-          loading1....
+          <CircularProgress/>
         </div>
       ) : (
         <Table striped bordered hover className={styles.table}>
